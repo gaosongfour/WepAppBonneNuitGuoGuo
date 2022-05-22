57f8f7652ff08d50f3ba41975ed89e6aec58606c
Song GAO <gaosongfour@hotmail.com>
2022-05-22 21:10:52 +0800

    Create a test controller api api/test/whoami

---
+++ WebAppBonneNuitGuoGuo.WebAPI/Controllers/Tests/TestController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAppBonneNuitGuoGuo.WebAPI.Models.Tests;
+
+namespace WebAppBonneNuitGuoGuo.WebAPI.Controllers.Tests
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        [Route("whoami")]
+        public ActionResult<WhoAmIResponse> WhoAmI()
+        {
+            var response = new WhoAmIResponse { UserId = "456433", UserName = "Song" };
+
+            return Ok(response);
+        }
+    }
+}
---
+++ WebAppBonneNuitGuoGuo.WebAPI/Models/Tests/WhoAmIModel.cs
@@ -0,0 +1,15 @@
+﻿namespace WebAppBonneNuitGuoGuo.WebAPI.Models.Tests
+{
+    public class WhoAmIResponse
+    {
+        /// <summary>
+        /// User Id
+        /// </summary>
+        public string? UserId { get; set; }
+
+        /// <summary>
+        /// User Name
+        /// </summary>
+        public string? UserName { get; set; }
+    }
+}
